35bf1550dbad5731bcf1f4233ad1a69923b71ded
SIST <SIST@DESKTOP-S8T2K8A>
2020-07-09 09:00:41 +0900

    200703

--- 20200703-3주차정리/src/연습문제ch4.java
+++ 20200703-3주차정리/src/연습문제ch4.java
@@ -23,26 +23,54 @@ public class 연습문제ch4 {
 				System.out.println(sum);
 				
 				//4-4
-				/*
-				sum=0;
-				int i=1;
 				
+				sum=0;
+				
+				int i=1;
 				while(true)
 				{
-					sum+=i;
 					
+					
+					if(i%2==0)
+						sum-=i;
+					else
+						sum+=i;
+
+				
+				
 					if(sum>=100)
 						break;
 					
 					i++;
-					i=-i;
-
+					
 				}
 				System.out.println(i);
+				
+				
+				
+				/*
+					 * sum+=i;
+					 * 
+					 * i++; i=-i;
+					 * 
+					 * if(sum>=100) break;
+					 */
+				/*
+				int sum = 0; // 총합을 저장할 변수
+				int s = 1; // 값의 부호를 바꿔주는데 사용할 변수
+				int num = 0;
+				// true . 조건식의 값이 이므로 무한반복문이 된다
+				for(int i=1;true; i++, s=-s) { // s 1, -1, 1, -1... 매 반복마다 의 값은
+				num = s * i; // i (s) . 와 부호 를 곱해서 더할 값을 구한다
+				sum += num;
+				if(sum >=100) // 100 . 총합이 보다 같거나 크면 반복문을 빠져 나간다
+				break;
+				}
+				System.out.println("num="+num);
 				*/
 				
 				//4-5
-				int i=0;
+				i=0;
 				while(i<=10)
 				{
 					
