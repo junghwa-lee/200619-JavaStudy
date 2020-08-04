b74d67d92962a24bd407d1bafdee4d2055d99286
SIST <SIST@DESKTOP-S8T2K8A>
2020-08-04 10:35:27 +0900

    200728

--- 20200728-자바프로젝트/src/com/sist/client/ClientMainFrame.java
+++ 20200728-자바프로젝트/src/com/sist/client/ClientMainFrame.java
@@ -1,18 +1,33 @@
 package com.sist.client;
 // 네이버영화
 import javax.swing.*; // 윈도우관련
+
+import com.sist.common.Function;
+
 import java.awt.*; // color, layout
 import java.awt.event.*; // interface
-
-public class ClientMainFrame extends JFrame implements ActionListener,MouseListener{ 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+/*
+ *   1. 사용자가 서버에 요청
+ *   2. 서버에서 들어오는 응답을 받아서 출력(쓰레드) => 자동화
+ */
+public class ClientMainFrame extends JFrame implements ActionListener,MouseListener,Runnable{ 
 	// panel=>mouselistener
 	// 윈도우 크기 결정 ==> 생성자에서 (291page)
 	JLabel title=new JLabel("영화 예매 & 추천 프로그램",JLabel.CENTER);
 	MenuForm mf=new MenuForm();
 	ChatForm cf=new ChatForm();
+	DetailForm df=new DetailForm();
+	Login login=new Login();
 	ControlPanel cp;
 	int curpage=1;
 	int totalpage=16;
+	//네트워크 관련 프로그램
+	Socket s; //연결기계
+	OutputStream out; // 서버로 요청값 전송 ==> 로그인, 채팅 문자열, 종료...
+	BufferedReader in; // 서버로부터 값을 받아오는 클래스  ==> 쓰레드
 	
 	public ClientMainFrame()
 	{
@@ -43,8 +58,10 @@ public class ClientMainFrame extends JFrame implements ActionListener,MouseListe
 		add(cp);
 		
 		setSize(1600, 1000);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);// X버튼 클릭 => 종료
+		//setVisible(true);
+		//setDefaultCloseOperation(EXIT_ON_CLOSE);// X버튼 클릭 => 종료
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE); //종료불가
+		
 		
 		mf.b1.addActionListener(this);
 		mf.b2.addActionListener(this);
@@ -63,6 +80,15 @@ public class ClientMainFrame extends JFrame implements ActionListener,MouseListe
 		cp.lf.b6.addActionListener(this);
 		cp.lf.tf.addActionListener(this);
 		
+		// 로그인 처리
+		login.b1.addActionListener(this);
+		login.b2.addActionListener(this);
+		
+		//종료
+		mf.b7.addActionListener(this);
+		
+		//채팅
+		cf.tf.addActionListener(this);
 		
 		/*
 		 * for(int i=0;i<10;i++) { cp.ff.mc[i].addMouseListener(this); }
@@ -158,6 +184,73 @@ public class ClientMainFrame extends JFrame implements ActionListener,MouseListe
 			}
 			cp.lf.movieFindData(ss);
 		}
+		
+		else if(e.getSource()==login.b1)
+		{
+			String id=login.tf1.getText();
+			if(id.length()<1) // id입력하지 않은 경우
+			{
+				JOptionPane.showMessageDialog(this, "아이디를 입력하세요");
+				login.tf1.requestFocus();
+				return;
+			}
+			
+			String name=login.tf2.getText();
+			if(name.length()<1) // id입력하지 않은 경우
+			{
+				JOptionPane.showMessageDialog(this, "대화명을 입력하세요");
+				login.tf2.requestFocus();
+				return;
+			}
+			
+			String sex="";
+			if(login.rb1.isSelected())
+				sex="남자";
+			else
+				sex="여자";
+			
+			// 서버로 입력받은 데이터 전송
+			try {
+				//연결
+				s=new Socket("localhost",5656);
+				//송수신 위치 확인
+				in=new BufferedReader(new InputStreamReader(s.getInputStream()));
+				out=s.getOutputStream();
+			
+				//로그인 요청(서버로 전송)
+				out.write((Function.LOGIN+"|"+id+"|"+name+"|"+sex+"\n").getBytes());
+			} catch (Exception ex) {}
+				
+			//서버에서 전송하는 데이터를 읽어서 출력
+			new Thread(this).start();
+			//run의 위치 확인 ==> 자신의 클래스 안에 존재 => (this)
+		}
+		
+		else if(e.getSource()==login.b2)
+		{
+			System.exit(0);
+		}
+		
+		else if(e.getSource()==cf.tf) //채팅
+		{
+			String msg=cf.tf.getText();
+			if(msg.length()<1)
+				return;
+			//데이터를 서버로 전송
+			try {
+				 out.write((msg+"\n").getBytes());
+			} catch (Exception ex) {}
+			
+			cf.tf.setText("");
+		}
+		
+		else if(e.getSource()==mf.b7)
+		{
+			try {
+				out.write((Function.EXIT+"|\n").getBytes());
+			} catch (Exception ex) {}
+			
+		}
 	}
 
 	@Override
@@ -171,8 +264,10 @@ public class ClientMainFrame extends JFrame implements ActionListener,MouseListe
 				{
 					int a=(i+1)+((curpage*10)-10);
 					//JOptionPane.showMessageDialog(this, "mno="+a);
-					cp.df.detailPrint(a);
-					cp.card.show(cp, "DF");
+					//cp.df.detailPrint(a);
+					df.detailPrint(a);
+					//cp.card.show(cp, "DF");
+					df.setVisible(true);
 					break;
 				}
 			}
@@ -208,4 +303,68 @@ public class ClientMainFrame extends JFrame implements ActionListener,MouseListe
  *  				movieform, detailform 은 한단계 거쳐야 접근가능 
  *  모든 액션은 clientmainframe에서 해야 함
  */
+
+	@Override
+	public void run() {
+		// 서버에서 들어오는 데이터를 받아서 처리
+		try {
+			while(true)
+			{
+				String msg=in.readLine();
+				System.out.println("server=>"+msg);
+				StringTokenizer st=new StringTokenizer(msg, "|");
+				int protocol=Integer.parseInt(st.nextToken());
+				switch(protocol)
+				{
+					case Function.MYLOG:
+					{
+						setTitle(st.nextToken());
+						login.setVisible(false); // 로그인창 
+						setVisible(true); // 영화창(Main)
+					}
+					break;
+					case Function.LOGIN:
+					{
+						//  테이블에 출력
+						String[] data= {
+								st.nextToken(), //id
+								st.nextToken(), //name
+								st.nextToken()  //sex
+						};
+						cf.model.addRow(data); 
+					}
+					break;
+					case Function.CHAT:
+					{
+						cf.ta.append(st.nextToken()+"\n");
+					}
+					break;
+					case Function.EXIT:
+					{
+						String id=st.nextToken();
+						for(int i=0;i<cf.model.getRowCount();i++)
+						{
+							String mid=cf.model.getValueAt(i, 0).toString();
+							if(id.equals(mid))
+							{
+								cf.model.removeRow(i);
+								break;
+							}
+						}
+					}
+					break;
+					case Function.MYEXIT:
+					{
+						dispose(); //윈도우 메모리 해제
+						System.exit(0); //프로그램 종료
+				
+					}
+					break;
+				}
+			}
+		} catch (Exception ex) {
+			// TODO: handle exception
+		}
+		
+	}
 }
